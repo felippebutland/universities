2060a6552fffe79520cb53f07243fbc2d99c4486
felippebutland <butlandfelippe@gmail.com>
2023-02-20 13:39:21 -0300

    fix: add authMiddleware in routes

--- src/modules/universities/controllers/university.controller.ts
+++ src/modules/universities/controllers/university.controller.ts
@@ -4,6 +4,7 @@ import {UniversityDTO, UpdateUniversityDTO} from "../domain/university.entity";
 import {createRepository, connect} from "../../../shared/connection-mongodb";
 import {UniversityRepository} from "../repository/university.repository";
 import { Collection } from "mongodb";
+import {authMiddleware} from "../../../shared/middleware/authentication.middleware";
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ createRepository().then(async () => {
 
     new UniversityRepository(universityCollection);
 
-    router.get("/", async (req: Request<{}, {}, {}, { country?: string; page?: string; limit?: string }>, res: Response) => {
+    router.get("/", authMiddleware, async (req: Request<{}, {}, {}, { country?: string; page?: string; limit?: string }>, res: Response) => {
         const {country, page, limit} = req.query;
         const universities = await getUniversitiesUseCase.execute({
             country,
@@ -29,12 +30,12 @@ createRepository().then(async () => {
         res.json(universities);
     });
 
-    router.post("/", async (req: Request<{}, {}, UniversityDTO>, res: Response) => {
+    router.post("/", authMiddleware, async (req: Request<{}, {}, UniversityDTO>, res: Response) => {
         const university = await createUniversityUseCase.execute(req.body);
         res.json(university);
     });
 
-    router.put("/:id", async (req: Request<{ id: string }, {}, UpdateUniversityDTO>, res: Response) => {
+    router.put("/:id", authMiddleware, async (req: Request<{ id: string }, {}, UpdateUniversityDTO>, res: Response) => {
         const {id} = req.params;
         const university = await updateUniversityUseCase.execute({
             ...req.body,
@@ -43,7 +44,7 @@ createRepository().then(async () => {
         res.json(university);
     });
 
-    router.delete("/:id", async (req: Request<{ id: string }, {}, {}>, res: Response) => {
+    router.delete("/:id", authMiddleware, async (req: Request<{ id: string }, {}, {}>, res: Response) => {
         const {id} = req.params;
         const university = await deleteUniversityUseCase.execute(new ObjectId(id));
         res.json(university);
